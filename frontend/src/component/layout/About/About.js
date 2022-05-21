b1908b037a3a6a89e386cd074c8052d4783c1dec
Dinesh Das <dineshdas1016@gmail.com>
2022-05-22 00:44:27 +0530

    initial

---
+++ frontend/src/component/layout/About/About.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "./aboutSection.css";
+import { Button, Typography, Avatar } from "@material-ui/core";
+import YouTubeIcon from "@material-ui/icons/YouTube";
+import InstagramIcon from "@material-ui/icons/Instagram";
+const About = () => {
+  const visitInstagram = () => {
+    window.location = "https://instagram.com/dinesh._.das";
+  };
+  return (
+    <div className="aboutSection">
+      <div></div>
+      <div className="aboutSectionGradient"></div>
+      <div className="aboutSectionContainer">
+        <Typography component="h1">About Us</Typography>
+
+        <div>
+          <div>
+            <Avatar
+              style={{ width: "10vmax", height: "10vmax", margin: "2vmax 0" }}
+              src="https://res.cloudinary.com/dvz8huo5a/image/upload/v1653037284/0b8336e0d2f0e6fee9c3f811896b4e5f_ohooqz.jpg"
+              alt="Founder"
+            />
+            <Typography>Dinesh Das</Typography>
+            <Button onClick={visitInstagram} color="primary">
+              Visit Instagram
+            </Button>
+            <span>
+              This is a sample ecommerce wesbite made by @teamCorizo. Only with the
+              purpose to learn
+            </span>
+          </div>
+          <div className="aboutSectionContainer2">
+            <Typography component="h2">Our Brands</Typography>
+            <a
+              href="https://www.youtube.com/"
+              target="blank"
+            >
+              <YouTubeIcon className="youtubeSvgIcon" />
+            </a>
+
+            <a href="https://instagram.com/dinesh._.das" target="blank">
+              <InstagramIcon className="instagramSvgIcon" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
